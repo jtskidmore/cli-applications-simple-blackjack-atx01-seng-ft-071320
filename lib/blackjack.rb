6d2c7a8c280cc31c01361d7f2ca0c04b61a73f96
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-10 23:29:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,78 +1,62 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
   rand(1..11)
 end
 
-def display_card_total (card_total)
-  # code #display_card_total here
-  puts "Your cards add up to #{card_total}"
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
   gets.chomp
 end
 
-def end_game (card_total)
-  # code #end_game here
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-  hand = deal_card+deal_card
-  display_card_total(hand)
-  return hand
+  sum = deal_card() + deal_card()
+  display_card_total(sum)
+  sum
 end
 
 def invalid_command
-  # code invalid_command here
   puts "Please enter a valid command"
 end
 
-def hit? (card_total)
-  # code hit? here
+def hit?(card_total)
   prompt_user()
-  user_input = get_user_input()
-
-  if user_input == 'h'
-    deal = deal_card()
-    card_total+=deal
-    return card_total
-  elsif user_input == 's'
-    return card_total
+  input = get_user_input()
+  if input == 'h'
+    new_card = deal_card()
+    card_total += new_card
+  elsif input == 's' 
+    nil
   else
     invalid_command
     prompt_user()
     get_user_input()
   end
-  
+  card_total
 end
 
-
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
 def runner
-  # code runner here
-  welcome
-  card_total = initial_round
-  until card_total > 21 do 
+  welcome()
+  card_total = initial_round()
+  until card_total > 21 do
     card_total = hit?(card_total)
-    display_card_total (card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
-    
+
